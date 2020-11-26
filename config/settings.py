32953cac926ef81b0e6807fe83963e657e560e2a
gusgkdl <gusgkdl2@naver.com>
2020-11-27 04:45:19 +0900

    commit!

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '6ax^amuvrfmq28^elpa^pluq8ew5f_an^woeovi9vl-l1o8=m%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
